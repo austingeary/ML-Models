17ca6df1c93896cc91ae57e93d6c685e0a595c5f
Austin Geary <austin.cg1@gmail.com>
2022-12-31 16:12:39 -0500

    K means

--- models_ag.py
+++ models_ag.py
@@ -32,7 +32,7 @@ class KNN:
 class LinearRegression:
     def __init__(self, iterations=100, learning_rate=0.01):
         self.iterations = iterations
-        self.learning_rate=learning_rate
+        self.learning_rate = learning_rate
         self.weights = None
         self.bias = None
 
@@ -55,7 +55,7 @@ class LinearRegression:
             #Predict Y value based on X values and current weights
             pred = self.predict_point(X[i])
             #Error
-            error = 2 * (y[i] - pred)
+            error = 2 * (pred - y[i])
             #Loop through all features of this point 
             for j in range(n_features):
                 #The gradient in the j dimension is the sum of
@@ -77,9 +77,9 @@ class LinearRegression:
         return linear_pred
 
     def update_params(self, gradient_bias, gradient_weights):
-        self.bias += gradient_bias * self.learning_rate
+        self.bias -= gradient_bias * self.learning_rate
         for i in range(len(gradient_weights)):
-            self.weights[i] += (gradient_weights[i] * self.learning_rate)
+            self.weights[i] -= (gradient_weights[i] * self.learning_rate)
         return None
 
 class LogisticRegression:
@@ -157,3 +157,72 @@ class LogisticRegression:
         for i in range(len(gradient_weights)):
             self.weights[i] -= (gradient_weights[i] * self.learning_rate)
         return None
+
+class KMeans:
+    def __init__(self, k, threshold):
+        self.k = k
+        self.threshold = threshold
+        self.centroids = None
+        self.labels = None
+
+    def fit(self, data):
+        self.centroids = self.intialize_centroids(data)
+
+        while True:
+            old_centroids = self.centroids
+            self.labels = self.get_labels(data)
+            self.centroids = self.update_centroids(data)
+
+            if self.should_stop(old_centroids):
+                break
+
+        return None
+
+    def intialize_centroids(self, data):
+        xmin = ymin = float('inf')
+        xmax = ymax = float('-inf')
+        for point in data:
+            xmin = min(point[0], xmin)
+            xmax = max(point[0], xmax)
+            ymin = min(point[1], ymin)
+            ymax = max(point[1], ymax)
+        centroids = []
+        for _ in range(self.k):
+            centroids.append([self.random_sample(xmin, xmax), self.random_sample(ymin, ymax)])
+        return centroids
+
+    def random_sample(self, low, high):
+        return low + (high - low) * random.random()
+
+    def get_labels(self, data):
+        labels = []
+        for point in data:
+            min_dist = float('inf')
+            label = None
+            for i, centroid in enumerate(self.centroids):
+                new_dist = funcs.get_distance(point, centroid)
+                if new_dist < min_dist:
+                    min_dist = new_dist
+                    label = i
+            labels.append(label)
+        return labels
+
+    def update_centroids(self, data):
+        new_centroids = [[0,0] for _ in range(self.k)]
+        counts = [0] * self.k
+        
+        for point, label in zip(data, self.labels):
+            new_centroids[label][0] += point[0]
+            new_centroids[label][1] += point[1]
+            counts[label] += 1
+            
+        for i, (x, y) in enumerate(new_centroids):
+            new_centroids[i] = (x / counts[i], y / counts[i])
+            
+        return new_centroids
+
+    def should_stop(self, old_centroids, threshold = 1e-5):
+        total_movement = 0
+        for old_point, new_point in zip(old_centroids, self.centroids):
+            total_movement += funcs.get_distance(old_point, new_point)
+        return total_movement < threshold
